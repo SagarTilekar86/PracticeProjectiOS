6ff70c7b224e9d2e61de913ad7c275df8169bb22
SagarTilekar86 <55085329+SagarTilekar86@users.noreply.github.com>
2021-10-20 15:55:20 +0530

    Update ContentView.swift

--- PracticeProject/PracticeProject/ContentView.swift
+++ PracticeProject/PracticeProject/ContentView.swift
@@ -5,7 +5,7 @@
 //  
 //
 /*
- **Note This code is develop on Xcode 12.4 not on Xcode 13 so their will some UI deviation on iOS 14.4+
+ **Note This code is developed on Xcode 12.4 not on Xcode 13 so their will some UI deviation on iOS 14.4+
  */
 
 import SwiftUI
